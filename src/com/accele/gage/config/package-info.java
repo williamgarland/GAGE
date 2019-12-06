f7548bf9d47039bb7eb6631383ffc23fdecb48ff
William Garland <garland.william99@gmail.com>
2019-12-05 21:20:24 -0500

    Added config package info

---
+++ src/com/accele/gage/config/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains classes used to modify and retrieve global engine settings as well as user-defined settings.
+ * 
+ * @author William Garland
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+package com.accele.gage.config;
