5ecb2d16168be0d888d65a91b932e4a980a772c7
William Garland <garland.william99@gmail.com>
2019-12-17 19:33:17 -0500

    Updated EntityHandler

--- src/com/accele/gage/entity/EntityHandler.java
+++ src/com/accele/gage/entity/EntityHandler.java
@@ -8,6 +8,7 @@ import com.accele.gage.GAGE;
 import com.accele.gage.GameConfiguration;
 import com.accele.gage.Renderable;
 import com.accele.gage.Tickable;
+import com.accele.gage.callbacks.EntityHandlerCallback;
 import com.accele.gage.control.ControlListener;
 import com.accele.gage.control.KeyListener;
 import com.accele.gage.control.MouseListener;
@@ -29,6 +30,8 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 
 	private List<Entity> entities;
 	private GameConfiguration config;
+	private List<EntityHandlerCallback> entityAddCallbacks;
+	private List<EntityHandlerCallback> entityRemoveCallbacks;
 	
 	/**
 	 * Creates a new {@link com.accele.gage.entity.EntityHandler EntityHandler} with the specified {@link com.accele.gage.GameConfiguration GameConfiguration}.
@@ -41,6 +44,8 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 	public EntityHandler(GameConfiguration config) {
 		this.entities = new ArrayList<>();
 		this.config = config;
+		this.entityAddCallbacks = new ArrayList<>();
+		this.entityRemoveCallbacks = new ArrayList<>();
 	}
 	
 	public void tick() {
@@ -69,14 +74,16 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 	
 	/**
 	 * Adds an {@link com.accele.gage.entity.Entity Entity} to the {@code EntityHandler}.
+	 * 
 	 * <p>
 	 * This method will wait to add the {@code Entity} until the end of the current game loop cycle.
+	 * When the {@code Entity} is actually added to the list, all entity-add callbacks will be invoked.
 	 * </p>
 	 * 
 	 * @param e the {@code Entity} to add
 	 */
 	public void addEntity(Entity e) {
-		GAGE.getInstance().deferEvent(gage -> entities.add(e));
+		GAGE.getInstance().deferEvent(gage -> { entities.add(e); entityAddCallbacks.forEach(c -> c.call(e)); });
 	}
 	
 	/**
@@ -85,6 +92,7 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 	 * This method will wait to remove the {@code Entity} until the end of the current game loop cycle.
 	 * This method also will not kill the {@code Entity} before removing it, so {@link com.accele.gage.entity.Entity#onDeath() onDeath()} will be ignored.
 	 * To properly kill an entity, use {@link com.accele.gage.entity.Entity#die() die()}.
+	 * When the {@code Entity} is actually removed from the list, all entity-remove callbacks will be invoked.
 	 * </p>
 	 * @param e the {@code Entity} to remove
 	 * @see com.accele.gage.entity.Entity#die() die()
@@ -98,6 +106,7 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 				gage.getMouseListenerRegistry().removeEntry(e.registryId);
 			if (e instanceof ControlListener && !(e instanceof KeyListener) && !(e instanceof MouseListener))
 				gage.getControlListenerRegistry().removeEntry(e.registryId);
+			entityRemoveCallbacks.forEach(c -> c.call(e));
 		});
 	}
 	
@@ -107,6 +116,7 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 	 * This method will wait to remove the entities until the end of the current game loop cycle.
 	 * This method also will not kill the entities before removing them, so {@link com.accele.gage.entity.Entity#onDeath() onDeath()} will be ignored.
 	 * To properly kill an entity, use {@link com.accele.gage.entity.Entity#die() die()}.
+	 * All entity-remove callbacks will be invoked for each entity in the list.
 	 * </p>
 	 * @see com.accele.gage.entity.Entity#die() die()
 	 */
@@ -119,6 +129,7 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 					gage.getMouseListenerRegistry().removeEntry(e.registryId);
 				if (e instanceof ControlListener && !(e instanceof KeyListener) && !(e instanceof MouseListener))
 					gage.getControlListenerRegistry().removeEntry(e.registryId);
+				entityRemoveCallbacks.forEach(c -> c.call(e));
 			});
 			entities.clear();
 		});
@@ -131,6 +142,24 @@ public class EntityHandler implements Tickable, Renderable, Cleanable {
 	 */
 	public List<Entity> getEntities() {
 		return entities;
+	}
+	
+	/**
+	 * Adds an entity-add callback to the {@code EntityHandler}. The callback will be invoked whenever an {@code Entity} is added to the {@code EntityHandler}.
+	 * 
+	 * @param callback the {@link com.accele.gage.callbacks.EntityHandlerCallback EntityHandlerCallback} to add
+	 */
+	public void addEntityAddCallback(EntityHandlerCallback callback) {
+		entityAddCallbacks.add(callback);
+	}
+	
+	/**
+	 * Adds an entity-remove callback to the {@code EntityHandler}. The callback will be invoked whenever an {@code Entity} is removed from the {@code EntityHandler}.
+	 * 
+	 * @param callback the {@link com.accele.gage.callbacks.EntityHandlerCallback EntityHandlerCallback} to add
+	 */
+	public void addEntityRemoveCallback(EntityHandlerCallback callback) {
+		entityRemoveCallbacks.add(callback);
 	}
 	
 }
