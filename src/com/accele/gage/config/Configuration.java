b8b54eb30d44c8695b28d5e780a92d760bcc042a
William Garland <garland.william99@gmail.com>
2020-01-21 14:47:06 -0500

    Fixed configuration getters

--- src/com/accele/gage/config/Configuration.java
+++ src/com/accele/gage/config/Configuration.java
@@ -82,7 +82,7 @@ public class Configuration implements Indexable {
 	 * @throws UnsupportedOperationException	if the {@code Configuration} does not allow adding properties
 	 * @throws IllegalArgumentException			if the {@code Configuration} already contains the specified {@code key}
 	 */
-	public void addProperty(String key, String value) {
+	public void add(String key, Object value) {
 		if (!canAddProperties)
 			throw new UnsupportedOperationException("Adding properties to configuration \"" + registryId + "\" is not allowed.");
 		if (properties.containsKey(key))
@@ -101,10 +101,157 @@ public class Configuration implements Indexable {
 	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
 	 * @return the value of the property with the specified {@code key}
 	 */
-	public Object getProperty(String key) {
+	public Object get(String key) {
 		if (!properties.containsKey(key))
 			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
 		return properties.get(key);
+	}
+	
+	/**
+	 * Retrieves the {@code byte} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain a {@code byte} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public Object getByte(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return Byte.parseByte(properties.get(key).toString());			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	/**
+	 * Retrieves the {@code short} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain a {@code short} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public Object getShort(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return Short.parseShort(properties.get(key).toString());			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	/**
+	 * Retrieves the {@code int} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain an {@code int} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public int getInt(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return Integer.parseInt(properties.get(key).toString());			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	/**
+	 * Retrieves the {@code long} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain a {@code long} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public long getLong(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return Long.parseLong(properties.get(key).toString());			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	/**
+	 * Retrieves the {@code float} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain a {@code float} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public float getFloat(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return Float.parseFloat(properties.get(key).toString());			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	/**
+	 * Retrieves the {@code double} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain a {@code double} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public double getDouble(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return Double.parseDouble(properties.get(key).toString());			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	/**
+	 * Retrieves the {@code char} property with the specified {@code key}.
+	 * 
+	 * <p>
+	 * This operation will fail if the {@code Configuration} does not contain a {@code char} property with the specified {@code key}.
+	 * </p>
+	 * 
+	 * @param key	the key of the property
+	 * @throws IllegalArgumentException	if the {@code Configuration} does not contain a property with the specified {@code key}
+	 * @return the value of the property with the specified {@code key}
+	 */
+	public char getChar(String key) {
+		if (!properties.containsKey(key))
+			throw new IllegalArgumentException("Invalid property key " + key + " for configuration \"" + registryId + "\".");
+		try {
+			return properties.get(key).toString().charAt(0);			
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
 	}
 	
 	/**
@@ -120,7 +267,7 @@ public class Configuration implements Indexable {
 	 * @throws IllegalArgumentException			if the {@code Configuration} does not contain a property with the specified {@code key}
 	 * @return the value of the removed property
 	 */
-	public Object removeProperty(String key) {
+	public Object remove(String key) {
 		if (!canRemoveProperties)
 			throw new UnsupportedOperationException("Removing properties from configuration \"" + registryId + "\" is not allowed.");
 		if (!properties.containsKey(key))
@@ -142,7 +289,7 @@ public class Configuration implements Indexable {
 	 * @throws IllegalArgumentException			if the {@code Configuration} does not contain a property with the specified {@code key}
 	 * @return the old value of the property
 	 */
-	public Object setProperty(String key, String newValue) {
+	public Object set(String key, Object newValue) {
 		if (!canSetProperties)
 			throw new UnsupportedOperationException("Setting properties in configuration \"" + registryId + "\" is not allowed.");
 		if (!properties.containsKey(key))
